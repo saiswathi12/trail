792910f8f02d0312c3fd77f652e54a6750ad5bcd
saiswathi12 <sswathi.devareddy@gmail.com>
2020-06-21 16:45:09 +0530

    new code

--- src/test/java/trail/trailtwo.java
+++ src/test/java/trail/trailtwo.java
@@ -2,7 +2,8 @@ package trail;
 
 public class trailtwo {
 	public static void main(String[] args) {
-		System.out.println("yet to be cloned");
+		System.out.println("yet to be clonee");
+		System.out.println("yet to be clonee");
 	}
 
 }
